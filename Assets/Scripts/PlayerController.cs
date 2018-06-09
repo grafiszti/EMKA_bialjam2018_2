7cd2d8f5314f2ad1c3449ac9247b993cb4a174dd
Mateusz Choinski <mchoinski@ownedoutcomes.com>
2018-06-09 11:30:13 +0200

    init commit, added player, simple floor and moving with animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    
+    public bool facingRight = true;
+    public bool jump = false;               
+
+    public float maxSpeed = 5f;
+    public float jumpForce = 10f;         // Amount of force added when the player jumps.
+    public float moveForce = 3650f;          // Amount of force added to move the player left and right.
+
+    private Transform groundCheck;          
+    private bool grounded = true; 
+    private Animator anim;
+    private Rigidbody2D body;
+
+    void Awake()
+    {
+        // Setting up references.
+        groundCheck = transform.Find("groundCheck");
+        anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+        //grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        // If the jump button is pressed and the player is grounded then the player should jump.
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+            //Debug.Log("JUMP");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Cache the horizontal input.
+        float h = Input.GetAxis("Horizontal");
+
+        // The Speed animator parameter is set to the absolute value of the horizontal input.
+        anim.SetFloat("Speed", Mathf.Abs(h));
+
+        // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+        if (h * body.velocity.x < maxSpeed)
+            // ... add a force to the player.
+            body.AddForce(Vector2.right * h * moveForce);
+
+        // If the player's horizontal velocity is greater than the maxSpeed...
+        if (Mathf.Abs(body.velocity.x) > maxSpeed)
+            // ... set the player's velocity to the maxSpeed in the x axis.
+            body.velocity = new Vector2(Mathf.Sign(body.velocity.x) * maxSpeed, body.velocity.y);
+
+        // Stopping down player when no horizontal input
+        if(Mathf.Abs(h)  < 0.001f){
+            //body.velocity = new Vector2(0, body.velocity.y);
+        }
+
+        // If the input is moving the player right and the player is facing left...
+        if (h > 0 && !facingRight)
+            Flip();
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (h < 0 && facingRight)
+            Flip();
+
+        if (jump)
+        {
+            // Set the Jump animator trigger parameter.
+            anim.SetTrigger("Jump");
+            Debug.Log("JebJEb");
+
+            // Play a random jump audio clip.
+            //int i = Random.Range(0, jumpClips.Length);
+            //AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
+
+            // Add a vertical force to the player.
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+
+            // Make sure the player can't jump again until the jump conditions from Update are satisfied.
+            jump = false;
+        }
+    }
+
+    void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    //make sure u replace "floor" with your gameobject name.on which player is standing
+    void OnCollisionEnter(Collision theCollision)
+    {
+        Debug.Log("Collision w: " + theCollision.gameObject.name);
+        //if (theCollision.gameObject.name == "floor")
+        //{
+            grounded = true;
+        //}
+    }
+
+    //consider when character is jumping .. it will exit collision.
+    void OnCollisionExit(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "floor")
+            grounded = false;
+    }
+}
