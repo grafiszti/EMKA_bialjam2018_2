3d0182173c3e4d8c72f26c5f36622ff4f1535641
Krzysiek Ruczaj <krj.public+github@gmail.com>
2018-06-10 12:17:52 +0200

    Added custom cursor support.

---
+++ Assets/Scripts/CursorSpriteController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorSpriteController : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+}
