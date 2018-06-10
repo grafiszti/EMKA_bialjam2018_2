768389f611df25af0d473f7989792931aec6ab1a
Krzysiek Ruczaj <krj.public+github@gmail.com>
2018-06-10 10:39:59 +0200

    Added health bar.

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+
+    private PlayerController knifeScript;
+    public GameObject player;
+    private Slider slider;
+
+    void Awake()
+    {
+        this.knifeScript = player.GetComponent<PlayerController>();
+        this.slider = GetComponentInChildren<Slider>();
+    }
+
+    void FixedUpdate()
+    {
+        float sharpness = knifeScript.sharpness;
+        slider.value = sharpness;
+    }
+}
