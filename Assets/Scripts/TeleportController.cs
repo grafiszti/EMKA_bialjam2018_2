c0d8f1932f73fa966c80c552a3a52a8ce0b1ea38
Mateusz Choinski <mchoinski@ownedoutcomes.com>
2018-06-10 04:38:46 +0200

    portals are working

---
+++ Assets/Scripts/TeleportController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour {
+    public GameObject outPortal;
+    public Vector2 spawnPoint;
+    public GameObject player;
+
+    public bool disabled = false;
+
+    public TeleportController outPortalController;
+
+    public void Awake()
+    {
+        outPortalController = outPortal.GetComponentInChildren<TeleportController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && !disabled){
+            outPortalController.disabled = true;
+            player.GetComponentInChildren<Rigidbody2D>().position = outPortalController.spawnPoint;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        disabled = false;
+    }
+}
